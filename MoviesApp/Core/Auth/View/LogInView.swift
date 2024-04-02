73cfc0853efc5bffa7797bf61e56a7350229d71b
ZacharyYongBB <zacharyyong_1997@outlook.com>
2024-04-02 15:32:03 +0800

    Initial Commit

---
+++ MoviesApp/Core/Auth/View/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  MoviesApp
+//
+//  Created by Zachary on 2/4/24.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
---
+++ MoviesApp/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Zachary on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LogInView()
+        }
+    }
+}
